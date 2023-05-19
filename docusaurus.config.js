db7e760e0ddc9b03cbc69e33764a850208c9e026
Aybars Göktuğ Ayan <aybarsgoktugayan@Aybarss-MacBook-Air.local>
2023-05-19 22:51:25 +0300

    test2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,10 +12,10 @@ const config = {
   plugins: ['docusaurus-plugin-sass'],
 
   // Set the production url of your site here
-  url: 'https://WidgetVRP.github.io',
+  url: 'https://sardisdoc.netlify.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/sardisdocs',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
